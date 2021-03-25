ea32285a8758e7057e1de53d54e88b26fa574a1b
calvo111979 <acalvoinfa10@gmail.com>
2021-03-25 16:42:14 +0100

    refactor: generaliza test

--- test/numberToRoman.test.ts
+++ test/numberToRoman.test.ts
@@ -1,23 +1,13 @@
 import { numberToRoman } from "../src/numberToRoman";
 
 describe("Número a romano", () => {
-  it("debe convertir el número 1 a I", () => {
-    expect(numberToRoman(1)).toBe("I");
-  });
-
-  it("debe convertir el número 2 a II", () => {
-    expect(numberToRoman(2)).toBe("II");
-  });
-
-  it("debe convertir el número 3 a III", () => {
-    expect(numberToRoman(3)).toBe("III");
-  });
-
-  it("devuelve IV para el número natural 4", () => {
-    expect(numberToRoman(4)).toBe("IV");
-  });
-
-  it("devuelve V para el número natural 5", () => {
-    expect(numberToRoman(5)).toBe("V");
+  it.each([
+    [1, "I"],
+    [2, "II"],
+    [3, "III"],
+    [4, "IV"],
+    [5, "V"],
+  ])("debe convertir el número %d a %s", (value, expected) => {
+    expect(numberToRoman(value)).toBe(expected);
   });
 });
