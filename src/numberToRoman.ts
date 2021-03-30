27a6ae1692fcffb53deca8714a8b362257b1b39d
calvo111979 <acalvoinfa10@gmail.com>
2021-03-30 10:19:52 +0200

    refactor: añade diccionario

--- src/numberToRoman.ts
+++ src/numberToRoman.ts
@@ -1,68 +1,78 @@
+const romanNumbers = {
+  1: "I",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
+
 export function numberToRoman(numero: number): string {
   let romanNumber = "";
-  let start = 0;
 
   if (numero >= 900) {
     numero -= 900;
-    romanNumber += "CM";
+    romanNumber += romanNumbers[900];
   }
 
   if (numero >= 500) {
     numero -= 500;
-    romanNumber += "D";
+    romanNumber += romanNumbers[500];
   }
 
   if (numero === 400) {
     numero -= 400;
-    romanNumber += "CD";
+    romanNumber += romanNumbers[400];
   }
 
   while (numero >= 100) {
     numero -= 100;
-    romanNumber += "C";
+    romanNumber += romanNumbers[100];
   }
 
   if (numero >= 90) {
     numero -= 90;
-    romanNumber += "XC";
+    romanNumber += romanNumbers[90];
   }
 
   if (numero >= 50) {
     numero -= 50;
-    romanNumber += "L";
+    romanNumber += romanNumbers[50];
   }
 
   if (numero >= 40) {
     numero -= 40;
-    romanNumber += "XL";
+    romanNumber += romanNumbers[40];
   }
 
   while (numero >= 10) {
     numero -= 10;
-    romanNumber += "X";
+    romanNumber += romanNumbers[10];
   }
 
   if (numero === 9) {
-    return (romanNumber += "IX");
+    return (romanNumber += romanNumbers[9]);
   }
 
   if (numero >= 5) {
     numero -= 5;
-    romanNumber += "V";
+    romanNumber += romanNumbers[5];
   }
 
   if (numero === 4) {
-    return (romanNumber += "IV");
+    return (romanNumber += romanNumbers[4]);
   }
 
-  return addStick(numero, romanNumber);
+  while (numero >= 1) {
+    numero -= 1;
+    romanNumber += romanNumbers[1];
+  }
+  return romanNumber;
 }
-
-const addStick = (numero: number, romanNumber: string): any => {
-  if (numero === 0) {
-    return romanNumber;
-  }
-
-  romanNumber += "I";
-  return addStick(numero - 1, romanNumber);
-};
