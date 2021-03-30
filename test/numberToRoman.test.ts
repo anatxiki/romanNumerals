130db781bebbbd35164b0274e0d911c9d7063d55
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-30 12:29:08 +0200

    refactor: elinima tests redundantes

--- test/numberToRoman.test.ts
+++ test/numberToRoman.test.ts
@@ -2,14 +2,10 @@ import { numberToRoman } from "../src/numberToRoman";
 
 describe("Número a romano", () => {
   it.each([
-    [1, "I"],
     [2, "II"],
-    [3, "III"],
     [4, "IV"],
     [5, "V"],
     [6, "VI"],
-    [7, "VII"],
-    [8, "VIII"],
     [9, "IX"],
     [10, "X"],
     [13, "XIII"],
