ffd8dd3e9d94d60344e8673897bef8d35fde16f0
Marcos Galvez <marcos.galvez@biko2.com>
2021-03-30 13:14:13 +0200

    refactor: elimina array de keys

--- src/numberToRoman.ts
+++ src/numberToRoman.ts
@@ -1,30 +1,28 @@
-const romanNumbers = {
-  1: "I",
-  4: "IV",
-  5: "V",
-  9: "IX",
-  10: "X",
-  40: "XL",
-  50: "L",
-  90: "XC",
-  100: "C",
-  400: "CD",
-  500: "D",
-  900: "CM",
-  1000: "M",
-};
-
-const breakNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const breakNumbers: Array<[number, string]> = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
 
 export function numberToRoman(numero: number): string {
-  let romanNumber = "";
+  let result = "";
 
-  for (let currentNumber of breakNumbers) {
-    while (numero >= currentNumber) {
-      numero -= currentNumber;
-      romanNumber += romanNumbers[currentNumber];
+  for (let [arabNumber, romanNumber] of breakNumbers) {
+    while (numero >= arabNumber) {
+      numero -= arabNumber;
+      result += romanNumber;
     }
   }
 
-  return romanNumber;
+  return result;
 }
